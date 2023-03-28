f0452f72446ee4c9493c9c05a8b392e963f7e06a
Thoma <thomaboehi@gmail.com>
2023-03-28 23:48:28 +0200

    Random changes

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ window.addEventListener('scroll', () => {
 });
 
 
-// PARTIE MAXIME
+/* -------------------------------------------------------------------------- */
+/*                                PARTIE MAXIME                               */
+/* -------------------------------------------------------------------------- */
+
+/* ---------------------------- ARTIST CAROUSSEL ---------------------------- */
 const artistContainer = document.querySelector(".artists-caroussel");
 const autoDefil = artistContainer.querySelector(".auto");
 const artistsElems = {
@@ -120,15 +124,27 @@ function displayArtist(i) {
 }
 
 
-// Maxime word slide - Need work
-// const names = document.querySelector(".names")
-// const specificNames = names.querySelectorAll("*")
-// console.log(specificNames)
-// let translate = 0;
+/* ------------------------------- NAME SLIDER ------------------------------ */
+const names = document.querySelector(".names");
+let specificNames = names.querySelectorAll("*");
+let translate = 0;
+let namesMargin = names.getBoundingClientRect().left;
 
-// function updateText() {
-//     translate += 1;
-//     names.style = "margin-left: -" + translate + "px"
-//     requestAnimationFrame(updateText)
-// }
-// requestAnimationFrame(updateText)
+function updateText() {
+    let delta = (Date.now() - lastUpdate)/1000; // delta time to have consistent speed regardless of refresh rates
+    lastUpdate = Date.now();
+    requestAnimationFrame(updateText);
+
+    translate += 70 * delta; // each seconds move from 80px
+    const rect = specificNames[1].getBoundingClientRect();
+    names.style = "margin-left: -" + translate + "px"; // apply pos
+    if (rect.left >= namesMargin) return; 
+    // to put the first element in last pos when then second start disapearing (why second and first idk i tested like this it work with the first one totaly disapeatring creating glitch)
+    names.appendChild(specificNames[0]);
+    specificNames = names.querySelectorAll("*"); // updates names array after change
+    translate = 0;
+    names.style = "margin-left: -" + translate + "px"; // apply pos
+    namesMargin = names.getBoundingClientRect().left; // doing this kind of overkill buut allow resize without reload (without start glitching)
+}
+let lastUpdate = Date.now();
+requestAnimationFrame(updateText);
