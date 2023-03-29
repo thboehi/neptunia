89b17236713b5cf59a6ec6a83d0dff96844d0a2e
Thoma <thomaboehi@gmail.com>
2023-03-29 10:46:01 +0200

    Changes by Maxime

--- index.js
+++ index.js
@@ -54,14 +54,62 @@ window.addEventListener('scroll', () => {
 /* -------------------------------------------------------------------------- */
 
 /* ---------------------------- ARTIST CAROUSSEL ---------------------------- */
-const artistContainer = document.querySelector(".artists-caroussel");
-const autoDefil = artistContainer.querySelector(".auto");
-const artistsElems = {
-    img: artistContainer.querySelector(".image > img"),
-    title: artistContainer.querySelector(".content > h2"),
-    text: artistContainer.querySelector(".content > p"),
-    social: artistContainer.querySelector(".content > .social-nav > a"),
-};
+const artistsContainer = document.querySelector(".artists-caroussel");
+const artists = artistsContainer.querySelectorAll(".content");
+const autoDefil = artistsContainer.querySelector(".auto");
+let actualSlide = 0;
+
+
+function changeSlide(to) { // change slide function declaration
+    // hide old image
+    artists[actualSlide].className = "content hidden";
+    // update slide
+    actualSlide += to;
+    if (actualSlide < 0) { actualSlide = artists.length-1 }
+    else if (actualSlide >= artists.length) { actualSlide = 0 }
+    // display new image
+    artists[actualSlide].className = "content active";
+}
+
+function play() { // auto slide function declaration
+    autoSlideInterval = setInterval(() => {
+        changeSlide(1);
+    }, 3000);
+    autoDefil.setAttribute("data-state", "play");
+}
+
+function pause() {  // stop auto slide function declaration
+    clearInterval(autoSlideInterval);
+    autoDefil.setAttribute("data-state", "pause");
+}
+
+
+/* ------------------------------- NAME SLIDER ------------------------------ */
+const names = document.querySelector(".names");
+let specificNames = names.querySelectorAll("*");
+let translate = 0;
+let namesMargin = names.getBoundingClientRect().left;
+
+function updateText() {
+    let delta = (Date.now() - lastUpdate)/1000; // delta time to have consistent speed regardless of refresh rates
+    lastUpdate = Date.now();
+    requestAnimationFrame(updateText);
+
+    translate += 70 * delta; // each seconds move from 80px
+    const rect = specificNames[1].getBoundingClientRect();
+    names.style = "margin-left: -" + translate + "px"; // apply pos
+    if (rect.left >= namesMargin) return; 
+    // to put the first element in last pos when then second start disapearing (why second and first idk i tested like this it work with the first one totaly disapeatring creating glitch)
+    names.appendChild(specificNames[0]);
+    specificNames = names.querySelectorAll("*"); // updates names array after change
+    translate = 0;
+    names.style = "margin-left: -" + translate + "px"; // apply pos
+    namesMargin = names.getBoundingClientRect().left; // doing this kind of overkill buut allow resize without reload (without start glitching)
+}
+let lastUpdate = Date.now();
+requestAnimationFrame(updateText);
+
+// no longer used left here to make sure no content is lefted out in HTML
 const artistSlides = [
     {
         img: "img/artists-slider/herrkrank.png",
@@ -94,57 +142,3 @@ const artistSlides = [
         soundCloud: "https://soundcloud.com"
     },
 ];
-let actualSlide = 0;
-
-function changeSlide(to) { // change slide function declaration
-    actualSlide += to;
-    if (actualSlide < 0) { actualSlide = artistSlides.length-1 }
-    else if (actualSlide >= artistSlides.length) { actualSlide = 0 }
-    displayArtist(actualSlide);
-}
-
-function play() { // auto slide function declaration
-    autoSlideInterval = setInterval(() => {
-        changeSlide(1);
-    }, 3000);
-    autoDefil.setAttribute("data-state", "play");
-}
-function pause() {  // stop auto slide function declaration
-    clearInterval(autoSlideInterval);
-    autoDefil.setAttribute("data-state", "pause");
-}
-
-function displayArtist(i) {
-    console.log(artistSlides);
-    artistsElems.img.src = artistSlides[i].img;
-    artistsElems.img.alt = `Image de l'artiste ${artistSlides[i].title}`
-    artistsElems.title.innerHTML = artistSlides[i].title;
-    artistsElems.text.innerHTML = artistSlides[i].text;
-    artistsElems.social.href = artistSlides[i].soundCloud;
-}
-
-
-/* ------------------------------- NAME SLIDER ------------------------------ */
-const names = document.querySelector(".names");
-let specificNames = names.querySelectorAll("*");
-let translate = 0;
-let namesMargin = names.getBoundingClientRect().left;
-
-function updateText() {
-    let delta = (Date.now() - lastUpdate)/1000; // delta time to have consistent speed regardless of refresh rates
-    lastUpdate = Date.now();
-    requestAnimationFrame(updateText);
-
-    translate += 70 * delta; // each seconds move from 80px
-    const rect = specificNames[1].getBoundingClientRect();
-    names.style = "margin-left: -" + translate + "px"; // apply pos
-    if (rect.left >= namesMargin) return; 
-    // to put the first element in last pos when then second start disapearing (why second and first idk i tested like this it work with the first one totaly disapeatring creating glitch)
-    names.appendChild(specificNames[0]);
-    specificNames = names.querySelectorAll("*"); // updates names array after change
-    translate = 0;
-    names.style = "margin-left: -" + translate + "px"; // apply pos
-    namesMargin = names.getBoundingClientRect().left; // doing this kind of overkill buut allow resize without reload (without start glitching)
-}
-let lastUpdate = Date.now();
-requestAnimationFrame(updateText);
